d1670b057c50ff35a17950ff3dd1eccbf83c877f
Ahmed Atef <ahmed.shera10@gmail.com>
2025-05-23 04:15:06 +0300

    End of section 6

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,42 @@
+﻿using API.DTOs;
+using Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuggyController : ControllerBase
+    {
+        [HttpGet("unauthorized")]
+        public IActionResult GetUnauthorized()
+        {
+            return Unauthorized();
+        }
+
+        [HttpGet("notfound")]
+        public IActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("badrequest")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest("Not agood request");
+        }
+
+        [HttpGet("internalerror")]
+        public IActionResult GetInternalError()
+        {
+            throw new Exception("This is a test exception");
+        }
+
+        [HttpPost("validationerror")]
+        public IActionResult GetValidationRrror(CreateProductDto product)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using API.Errors;
+using System.Net;
+using System.Text.Json;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware(IHostingEnvironment env,RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandelExceptionAsync(context,ex,env);
+            }
+        }
+
+        private static Task HandelExceptionAsync(HttpContext context, Exception ex, IHostingEnvironment env)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode=(int)HttpStatusCode.InternalServerError;
+            var response = env.IsDevelopment()
+                ? new ApiErrorResponse(context.Response.StatusCode, ex.Message, ex.StackTrace) :
+                new ApiErrorResponse(context.Response.StatusCode, ex.Message, "Internal server error");
+            var options=new JsonSerializerOptions { PropertyNamingPolicy=JsonNamingPolicy.CamelCase };
+            var json =JsonSerializer.Serialize(response, options);
+            return context.Response.WriteAsync(json);
+        }
+    }
+}
